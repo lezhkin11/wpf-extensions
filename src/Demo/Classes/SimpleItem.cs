bb7a6ad43322a618b7ce25f59ca9c2a7b1545a2c
oleg.slepov <os@exmaple.com>
2020-04-15 21:30:33 +0100

    Add demo project

---
+++ src/Demo/Classes/SimpleItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo.Classes
+{
+    public class SimpleItem
+    {
+        public SimpleItem(int number, string codeText)
+        {
+            Number = number;
+            CodeText = codeText;
+        }
+
+        public int Number { get; }
+
+        public string CodeText { get; }
+    }
+}
---
+++ src/Demo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using Demo.Classes;
+
+namespace Demo.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<SimpleItem>();
+
+            Items.Add(new SimpleItem(8, "11"));
+            Items.Add(new SimpleItem(2, "100"));
+            Items.Add(new SimpleItem(3, "X3"));
+            Items.Add(new SimpleItem(4, "1"));
+            Items.Add(new SimpleItem(4, "33"));
+            Items.Add(new SimpleItem(9, "23"));
+            Items.Add(new SimpleItem(9, "23UX"));
+            Items.Add(new SimpleItem(7, "2"));
+            Items.Add(new SimpleItem(9, "J23P92H"));
+        }
+
+        public ObservableCollection<SimpleItem> Items { get; }
+    }
+}
